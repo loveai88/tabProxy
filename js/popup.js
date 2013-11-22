ad0e4eebf5ae454d08e82f29a9efd9987343852c
loveai88 <luq.java@gmail.com>
2013-11-22 17:34:13 +0800

    Signed-off-by: loveai88 <luq.java@gmail.com>

---
+++ js/popup.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    init();
+});
+
+
+function init(){
+    jQuery('#settingBtn').click(setting);
+
+    
+}
+
+function setting(){
+    var background=chrome.extension.getBackgroundPage();
+
+}
